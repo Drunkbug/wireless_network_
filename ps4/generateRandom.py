8cc5afc7a16088887fd7c6543327b899222b2fb4
drunkbug <leyiqiang@gmail.com>
2017-04-08 19:54:42 -0400

    bug fix

--- ps4/generateRandom.py
+++ ps4/generateRandom.py
@@ -5,7 +5,7 @@ import numpy
 
 #TODO in readme: pip install numpy, use python 3
 
-def generate_noise_array(mean, snr):
+def generate_noise_array(mean, snr, size):
     """ generate noise array based on gaussian noise form 
 
     Args:
@@ -13,7 +13,7 @@ def generate_noise_array(mean, snr):
         snr: A number represents Eb/N0
     """
     stddev = (1/(snr*2))**(1/2)
-    noise = numpy.random.normal(mean, stddev, 1000000)
+    noise = numpy.random.normal(mean, stddev, size)
     return noise
 
 def generate_noise_sequence(signal_scheme, noise):
@@ -43,35 +43,47 @@ def get_bit_error_rate(sent_sequence, received_sequence, size):
     """
     acc = 0
     for s, r in zip(sent_sequence, received_sequence):
-        if s == r:
+        if s != r:
             acc += 1
 
     ber = acc / size
     return ber
 
-def initialize_scheme(size):
+def generate_sent_sequence(size):
     #TODO add bit rate
-    """ initialize scheme with bit rate
+    """ generate sent sequence
     Generates 10 million bits random 0s and 1s with given bit rate
-    Then map 0 to -1 and 1 to +1
 
     Args:
-        size: An integer for the size of the sequence
+        size: A integer indicates how many bits are in the sequence
         bit_rate: A number represents bit rate 
     """
     # generate binary sequency with bit rate 1/2
-    binary_sequence = [randint(0,1) for i in range(size)]
+    sent_sequence = [randint(0,1) for i in range(size)]
+    return sent_sequence
+
+
+def initialize_scheme(sent_sequence):
+    """ initialize scheme with bit rate
+    Map 0 to -1 and 1 to +1
+
+    Args:
+        sent_sequence: An arrary represent the sent sequence
+    """
     # generate antipodal signaling shceme
-    signal_scheme = [ (b - 1) if b == 0 else b for b in binary_sequence ]
+    signal_scheme = [ (b - 1) if b == 0 else b for b in sent_sequence]
     return signal_scheme
 
 
 if __name__ == "__main__":
+    # sent
     size = 1000000
-    sent_sequence = initialize_scheme(size)
-    noise = generate_noise_array(0, 3)
-    noise_sequence = generate_noise_sequence(sent_sequence, noise) 
+    sent_sequence = generate_sent_sequence(size)
+    signal_scheme = initialize_scheme(sent_sequence)
+    # noise
+    noise = generate_noise_array(0, 3, size)
+    noise_sequence = generate_noise_sequence(signal_scheme, noise) 
+    # receive
     received_sequence = generate_received_sequence(noise_sequence)
     ber = get_bit_error_rate(sent_sequence, received_sequence, size)
-    print (ber)
     
