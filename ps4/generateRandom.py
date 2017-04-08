1e9ae4bf3d46056cc8605dc51d5896e08ffcbd52
drunkbug <leyiqiang@gmail.com>
2017-04-08 19:19:21 -0400

    generate received sequence

--- ps4/generateRandom.py
+++ ps4/generateRandom.py
@@ -26,6 +26,14 @@ def generate_noise_sequence(signal_scheme, noise):
     """
     return [ s + n for s, n in zip(signal_scheme, noise) ]
 
+def generate_received_sequence(noise_sequence):
+    """ generate received sequence by using optimal detection rule
+
+    Args:
+        noise_sequence: An array represent the noise sequence
+    """
+    return [ 1 if n > 0 else 0 for n in noise_sequence ]
+
 def initialize_scheme():
     #TODO add bit rate
     """ initialize scheme with bit rate
@@ -38,7 +46,7 @@ def initialize_scheme():
     # generate binary sequency with bit rate 1/2
     binary_sequence = [randint(0,1) for i in range(1000000)]
     # generate antipodal signaling shceme
-    signal_scheme = [(b - 1) if b == 0 else b for b in binary_sequence]
+    signal_scheme = [ (b - 1) if b == 0 else b for b in binary_sequence ]
     return signal_scheme
 
 
@@ -47,5 +55,6 @@ if __name__ == "__main__":
     signal_scheme = initialize_scheme()
     noise = generate_noise_array(0, 3)
     noise_sequence = generate_noise_sequence(signal_scheme, noise) 
-    print (noise_sequence)
+    received_sequence = generate_received_sequence(noise_sequence)
+    print (received_sequence)
     
